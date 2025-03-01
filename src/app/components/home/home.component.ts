365e452b35c814b287b726ded0e483151884c1dd
Olivier Leblond <oleblond@psi-it.com>
2025-03-01 07:10:00 -0500

    Cleaned menu

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,27 +1,30 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { CommonModule, NgOptimizedImage } from "@angular/common";
-import { AuthService } from 'src/app/services/auth.service';
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
   standalone: true,
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
   imports: [CommonModule, NgOptimizedImage],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
   username!: string | null;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) {}
 
   ngOnInit(): void {
     this.username = this.authService.getFullname();
   }
 
   navigateTo(page: string): void {
-    this.router.navigate([`/${page}`]);
+    void this.router.navigate([`/${page}`]);
   }
 
   logout(): void {
